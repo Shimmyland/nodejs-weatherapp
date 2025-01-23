3a49e923d5ba19a8ce2a9b7e81056b3062854b01
Simon Libiger <116663110+Shimmyland@users.noreply.github.com>
2025-01-23 19:32:18 +0100

    REFACTOR: wrap existing queries in transactions

--- src/database/repositories/weathers.ts
+++ src/database/repositories/weathers.ts
@@ -1,20 +1,56 @@
 import { WeatherModel } from '../models/Weather.js'
 import { WeatherDto } from '../models/Weather.dto.js'
-
+import sequelize from '../database.js'
 
 export default Object.freeze({
-    create: async (dto: Omit<WeatherDto, 'id'>) => await WeatherModel.create(dto),
-    read: async(id:number) => {
+    create: async (dto: Omit<WeatherDto, 'id'>) => {
+        const transaction = await sequelize.transaction()
+        try {
+            await WeatherModel.create(dto, { transaction })
+            await transaction.commit()
+        } catch (error) {
+            await transaction.rollback()
+            throw error
+        }
+    },
+    read: async (id: number) => {
         return await WeatherModel.findByPk(id)
     },
-    readAll: async (limit:number, offset:number) => {
+    readAll: async (limit: number, offset: number) => {
         return await WeatherModel.findAll({
-          limit: limit,
-          offset: offset,
-          order: [['id', 'ASC']]
+            limit: limit,
+            offset: offset,
+            order: [['id', 'ASC']]
         })
     },
-    update: async(id:number, dto: Omit<WeatherDto, 'id'>) => await WeatherModel.update(dto, {where: {id: id}}),
-    updatePartial: async(id:number, dto: Partial<Omit<WeatherDto, 'id'>>) => await WeatherModel.update(dto, {where: {id: id}}),
-    delete: async(id:number) => await WeatherModel.destroy({where: {id: id}})
+    update: async (id: number, dto: Omit<WeatherDto, 'id'>) => {
+        const transaction = await sequelize.transaction()
+        try {
+            await WeatherModel.update(dto, { where: { id: id }, transaction })
+            await transaction.commit()
+        } catch (error) {
+            await transaction.rollback()
+            throw error
+        }
+    },
+    updatePartial: async (id: number, dto: Partial<Omit<WeatherDto, 'id'>>) => {
+        const transaction = await sequelize.transaction()
+        try {
+            await WeatherModel.update(dto, { where: { id: id }, transaction })
+            await transaction.commit()
+        } catch (error) {
+            await transaction.rollback()
+            throw error
+        }
+    },
+    delete: async (id: number) => {
+        const transaction = await sequelize.transaction()
+        try {
+            await WeatherModel.destroy({ where: { id: id }, transaction })
+            await transaction.commit()
+        } catch (error) {
+            await transaction.rollback()
+            throw error
+        }
+    }
 })
